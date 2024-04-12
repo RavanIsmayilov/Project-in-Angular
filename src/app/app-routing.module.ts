e3a1f26bb1727e8bc66030e63adf48d9ae7e7257
RavanIsmayilov <rvanismayilov70@gmail.com>
2024-04-12 19:47:44 +0400

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { WomenComponent } from './components/women/women.component';
+import { MenComponent } from './components/men/men.component';
+import { ProductsComponent } from './components/products/products.component';
+import { AboutComponent } from './components/about/about.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component:LayoutComponent,
+    children:[
+      {
+        path: "home",
+        component:HomeComponent
+      },
+      {
+        path: "women",
+        component:WomenComponent
+      },
+      {
+        path: "men",
+        component:MenComponent
+      },
+      {
+        path: "products",
+        component:ProductsComponent
+      },
+      {
+        path: "about",
+        component:AboutComponent
+      },
+      {
+        path: "**",
+        component:NotfoundComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WomenComponent } from './components/women/women.component';
+import { MenComponent } from './components/men/men.component';
+import { ProductsComponent } from './components/products/products.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { HomeComponent } from './components/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WomenComponent,
+    MenComponent,
+    ProductsComponent,
+    AboutComponent,
+    FooterComponent,
+    HeaderComponent,
+    NotfoundComponent,
+    HomeComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
